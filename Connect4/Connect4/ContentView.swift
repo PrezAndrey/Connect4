e1bc1c12b1654efcc37b03bfbebdb254fa1cb4ac
Andrey Prez <aw06.1994@yandex.ru>
2024-03-06 23:11:43 +0300

    Project was added

---
+++ Connect4/Connect4/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Connect4
+//
+//  Created by Андрей  on 06.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
