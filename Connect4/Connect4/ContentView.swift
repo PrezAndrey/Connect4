583e53762fa902b804bcf32e917ad85e6e693a11
Andrey Prez <aw06.1994@yandex.ru>
2024-03-21 22:03:08 +0300

    Fixing slot collection logic

--- Connect4/Connect4/ContentView.swift
+++ Connect4/Connect4/ContentView.swift
@@ -15,43 +15,44 @@ struct ContentView: View {
                                GridItem(.flexible()),
                                GridItem(.flexible())]
     // [[1, 2, 3, 4, 5, 6],  ... 36]
+    @State private var lastSlot: Slot? = nil
     @State private var moves: [Move?] = Array(repeating: nil, count: 36)
     @State private var turn: Bool = true
-    @State private var slots: Array<Slot> = [Slot(boardIndex: 0, filled: nil, columnIndex: 0),
-                                             Slot(boardIndex: 6, filled: nil, columnIndex: 0),
-                                             Slot(boardIndex: 12, filled: nil, columnIndex: 0),
-                                             Slot(boardIndex: 18, filled: nil, columnIndex: 0),
-                                             Slot(boardIndex: 24, filled: nil, columnIndex: 0),
-                                             Slot(boardIndex: 30, filled: nil, columnIndex: 0),
+    @State private var slots: Array<Slot> = [Slot(boardIndex: 0, filled:                                            nil, columnIndex: 0),
                                              Slot(boardIndex: 1, filled: nil, columnIndex: 1),
-                                             Slot(boardIndex: 7, filled: nil, columnIndex: 1),
-                                             Slot(boardIndex: 13, filled: nil, columnIndex: 1),
-                                             Slot(boardIndex: 19, filled: nil, columnIndex: 1),
-                                             Slot(boardIndex: 25, filled: nil, columnIndex: 1),
-                                             Slot(boardIndex: 31, filled: nil, columnIndex: 1),
                                              Slot(boardIndex: 2, filled: nil, columnIndex: 2),
-                                             Slot(boardIndex: 8, filled: nil, columnIndex: 2),
-                                             Slot(boardIndex: 14, filled: nil, columnIndex: 2),
-                                             Slot(boardIndex: 20, filled: nil, columnIndex: 2),
-                                             Slot(boardIndex: 26, filled: nil, columnIndex: 2),
-                                             Slot(boardIndex: 32, filled: nil, columnIndex: 2),
                                              Slot(boardIndex: 3, filled: nil, columnIndex: 3),
-                                             Slot(boardIndex: 9, filled: nil, columnIndex: 3),
-                                             Slot(boardIndex: 15, filled: nil, columnIndex: 3),
-                                             Slot(boardIndex: 21, filled: nil, columnIndex: 3),
-                                             Slot(boardIndex: 27, filled: nil, columnIndex: 3),
-                                             Slot(boardIndex: 33, filled: nil, columnIndex: 3),
                                              Slot(boardIndex: 4, filled: nil, columnIndex: 4),
-                                             Slot(boardIndex: 10, filled: nil, columnIndex: 4),
-                                             Slot(boardIndex: 16, filled: nil, columnIndex: 4),
-                                             Slot(boardIndex: 22, filled: nil, columnIndex: 4),
-                                             Slot(boardIndex: 28, filled: nil, columnIndex: 4),
-                                             Slot(boardIndex: 34, filled: nil, columnIndex: 4),
                                              Slot(boardIndex: 5, filled: nil, columnIndex: 5),
+                                             Slot(boardIndex: 6, filled: nil, columnIndex: 0),
+                                             Slot(boardIndex: 7, filled: nil, columnIndex: 1),
+                                             Slot(boardIndex: 8, filled: nil, columnIndex: 2),
+                                             Slot(boardIndex: 9, filled: nil, columnIndex: 3),
+                                             Slot(boardIndex: 10, filled: nil, columnIndex: 4),
                                              Slot(boardIndex: 11, filled: nil, columnIndex: 5),
+                                             Slot(boardIndex: 12, filled: nil, columnIndex: 0),
+                                             Slot(boardIndex: 13, filled: nil, columnIndex: 1),
+                                             Slot(boardIndex: 14, filled: nil, columnIndex: 2),
+                                             Slot(boardIndex: 15, filled: nil, columnIndex: 3),
+                                             Slot(boardIndex: 16, filled: nil, columnIndex: 4),
                                              Slot(boardIndex: 17, filled: nil, columnIndex: 5),
+                                             Slot(boardIndex: 18, filled: nil, columnIndex: 0),
+                                             Slot(boardIndex: 19, filled: nil, columnIndex: 1),
+                                             Slot(boardIndex: 20, filled: nil, columnIndex: 2),
+                                             Slot(boardIndex: 21, filled: nil, columnIndex: 3),
+                                             Slot(boardIndex: 22, filled: nil, columnIndex: 4),
                                              Slot(boardIndex: 23, filled: nil, columnIndex: 5),
+                                             Slot(boardIndex: 24, filled: nil, columnIndex: 0),
+                                             Slot(boardIndex: 25, filled: nil, columnIndex: 1),
+                                             Slot(boardIndex: 26, filled: nil, columnIndex: 2),
+                                             Slot(boardIndex: 27, filled: nil, columnIndex: 3),
+                                             Slot(boardIndex: 28, filled: nil, columnIndex: 4),
                                              Slot(boardIndex: 29, filled: nil, columnIndex: 5),
+                                             Slot(boardIndex: 30, filled: nil, columnIndex: 0),
+                                             Slot(boardIndex: 31, filled: nil, columnIndex: 1),
+                                             Slot(boardIndex: 32, filled: nil, columnIndex: 2),
+                                             Slot(boardIndex: 33, filled: nil, columnIndex: 3),
+                                             Slot(boardIndex: 34, filled: nil, columnIndex: 4),
                                              Slot(boardIndex: 35, filled: nil, columnIndex: 5),
     
     ]
@@ -69,7 +70,24 @@ struct ContentView: View {
                                 .foregroundColor(.white)
                         })
                         .onTapGesture {
-                            moves[i] = Move(player: determineTurn(), boardIndex: i)
+                            let tappedCol = slots[i].columnIndex
+                            
+                            for slot in slots {
+    
+                                if slot.columnIndex == tappedCol {
+                                    if slot.filled == nil {
+                                        lastSlot = slot
+                                    }
+                                    else {
+                                        if lastSlot != nil {
+                                            lastSlot?.filled = Move(player: determineTurn(), boardIndex: i)
+                                        }
+                                    }
+                                    
+                                }
+                            }
+                            lastSlot = nil
+                            //slots[i] = Move(player: determineTurn(), boardIndex: i)
                             for s in 0..<slots.count {
                                 if slots[s].boardIndex == i {
                                     if slots[s].filled != nil {
